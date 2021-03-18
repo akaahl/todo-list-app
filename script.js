3dc8a5126b4f2a347521091693a41dc7b5de9443
a.k.a.a.h.l <a.k.afiq@live.com>
2021-03-18 20:26:05 +0800

    Added todo functionality

--- script.js
+++ script.js
@@ -1,31 +1,86 @@
-// Drag and drop feature
-const individualList = document.querySelectorAll(".individual-list");
-const listContainer = document.querySelectorAll(".list-container");
+// Create todos
+const formContainer = document.getElementById("form-container");
+const ulContainer = document.getElementById("ul-container");
+const footerItems = document.getElementById("footer-items");
+const itemsLeft = document.getElementById("no-of-items");
+let inputText = document.getElementById("create-list-text");
 
-individualList.forEach((list) => {
-  list.addEventListener("dragstart", () => {
-    list.classList.add("dragging");
-  });
+function updateItemsLeft() {
+  itemsLeft.innerHTML = `${ulContainer.childElementCount - 1} items left`;
+}
 
-  list.addEventListener("dragend", () => {
-    list.classList.remove("dragging");
-  });
+updateItemsLeft();
+
+formContainer.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (inputText.value) {
+    let liContainer = document.createElement("li");
+    liContainer.classList.add("list-container");
+    liContainer.innerHTML = `
+        <div class="individual-list" draggable="true">
+                <input type="checkbox" name="list-items-checkbox" class="cross-checklist"/>
+                <img src="images/icon-check.svg" alt="check-icon" />
+                <div class="todo-items">
+                  <span>${inputText.value}</span>
+                  <img
+                    src="images/icon-cross.svg"
+                    alt="cross-icon"
+                    class="delete-btn"
+                  />
+                </div>
+              </div>
+    `;
+
+    ulContainer.insertBefore(liContainer, footerItems);
+    inputText.value = "";
+    dragAndDrop();
+    updateItemsLeft();
+    updateChecklist();
+  }
 });
 
-listContainer.forEach((container) => {
-  container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    const afterElement = getDragAfterElement(container, e.clientY);
-    const draggable = document.querySelector(".dragging");
-
-    if (afterElement == null) {
-      container.appendChild(draggable);
-    } else {
-      container.insertBefore(draggable, afterElement);
+function updateChecklist() {
+  const checkbox = document.querySelectorAll(".cross-checklist");
+  checkbox.forEach((item) => {
+    if (item.checked) {
+      //   updateItemsLeft();
+      console.log(true);
     }
   });
-});
+}
 
+// Drag and drop feature
+function dragAndDrop() {
+  const individualList = document.querySelectorAll(".individual-list");
+  const listContainer = document.querySelectorAll(".list-container");
+
+  individualList.forEach((list) => {
+    list.addEventListener("dragstart", () => {
+      list.classList.add("dragging");
+    });
+
+    list.addEventListener("dragend", () => {
+      list.classList.remove("dragging");
+    });
+  });
+
+  listContainer.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientY);
+      const draggable = document.querySelector(".dragging");
+
+      if (afterElement == null) {
+        container.appendChild(draggable);
+      } else {
+        container.insertBefore(draggable, afterElement);
+      }
+    });
+  });
+}
+
+// Track the mouse Y position between elements
 function getDragAfterElement(container, y) {
   const draggableElements = [
     ...container.querySelectorAll(".individual-list:not(.dragging)"),
