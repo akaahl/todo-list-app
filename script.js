535810ce961cf99ed7cf5f2276080d9634fd45e5
a.k.a.a.h.l <a.k.afiq@live.com>
2021-03-18 17:25:54 +0800

    Added drag and drop feature

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-const individualList = document.querySelectorAll('.individual-list');
-const listContainer = document.getElementById('list-container');
+// Drag and drop feature
+const individualList = document.querySelectorAll(".individual-list");
+const listContainer = document.querySelectorAll(".list-container");
 
-individualList.forEach(list => {
-    list.addEventListener('dragstart', () => {
-        list.classList.add('dragging');
-    });
+individualList.forEach((list) => {
+  list.addEventListener("dragstart", () => {
+    list.classList.add("dragging");
+  });
 
-    list.addEventListener('dragend', () => {
-        list.classList.remove('dragging');
-    });
+  list.addEventListener("dragend", () => {
+    list.classList.remove("dragging");
+  });
 });
 
-listContainer.addEventListener('dragover', e => {
+listContainer.forEach((container) => {
+  container.addEventListener("dragover", (e) => {
     e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
+    const draggable = document.querySelector(".dragging");
 
-    const afterElement = getDragAfterElement(listContainer, e.clientY);
-    const draggable = document.querySelector('.dragging');
-
-    console.log(afterElement);
-})
-
-
-console.log()
+    if (afterElement == null) {
+      container.appendChild(draggable);
+    } else {
+      container.insertBefore(draggable, afterElement);
+    }
+  });
+});
 
 function getDragAfterElement(container, y) {
-    const draggableElements = [...listContainer.querySelectorAll('.individual-list:not(.dragging)')];
+  const draggableElements = [
+    ...container.querySelectorAll(".individual-list:not(.dragging)"),
+  ];
 
-    return draggableElements.reduce((closest, child) => {
-        const box = child.getBoundingClientRect();
-        const offset = y - box.top - box.height / 2;
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
 
-        if (offset < 0 && offset > closest.offset) {
-            return { offset: offset, element: child };
-        } else {
-            return closest;
-        }
-
-    }, { offset: Number.POSITIVE_INFINITY}).element;
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
